2571a8733cbb4ddbb8aa1a2aeb76108f30ee8f4f
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-03-27 05:52:29 +0600

    add new style Question part

--- src/component/QuestionAnswer/QuestionAnswer.js
+++ src/component/QuestionAnswer/QuestionAnswer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import "./QuestionAnswer.css"
 
 const QuestionAnswer = () => {
     return (
-        <div>
+        <div className='questionBox'>
             <h2>Question Answer</h2>
 
             <div className="answer">
@@ -12,8 +13,8 @@ const QuestionAnswer = () => {
                 <p>রিয়েক্ট সর্বদাই একমুখী ডেটা প্রবাহ করে থাকে, রিয়েক্ট Real Dom এর ঠিক ফটোকপি Virtual Dom তৈরি করে তারপরে Real Dom এর সাথে Compare করে শুধু মাত্র যতোটুকো item change করা দরকার ততোটুকো item change করে দেয়।</p>
             </div>
 
-            <div className="answerCard ans-2">া
-                <h3>Props vs state এ ভিতরের পার্থক্য</h3>
+            <div className="answerCard ans-2">
+                <h3>Props vs State এ ভিতরের পার্থক্য</h3>
                 <p>State একটি আপডেটযোগ্য কাঠামো যা উপাদান সম্পর্কে ডেটা বা তথ্য ধারণ করতে ব্যবহৃত হয় এবং সময়ের সাথে সাথে change হতে পারে, আর Props শুধুমাত্র পঠনযোগ্য উপাদান। এটি এমন একটি Object যা ট্যাগের attribute মান Save করে এবং HTML attribute মতো কাজ করে।</p>
             </div>
 
